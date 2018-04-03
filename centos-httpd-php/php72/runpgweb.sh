942ccfcba9aafe8a91b244b8fc481fd63aa4e17d
crusiechang <changcruise@gmail.com>
2018-04-03 15:38:00 +0800

    [modify] modify ipalias

--- centos-httpd-php/php72/runpgweb.sh
+++ centos-httpd-php/php72/runpgweb.sh
@@ -1,3 +1,1 @@
-
-# docker run -dit --name pgweb -p 80:80   -v /Users/cruise/PHPProjects/test:/var/www/html cruisechang/centos7-httpd-php:php72
-docker run -dit --name pgweb -p 80:80   -v /Users/cruise/PHPProjects/test:/var/www/html cruisechang/centos-httpd-php:72
+docker run -dit --name web -p 80:80   -v /Users/PHPProjects/test:/var/www/html kumay/centos-httpd-php:72
